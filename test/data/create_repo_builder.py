c60d992f4b3727c8c5afe472f08384a8b04359a9
ElevateStuff <33041794+ElevateStuff@users.noreply.github.com>
2021-12-30 14:09:05 -0700

    add faker library

--- test/data/create_repo_builder.py
+++ test/data/create_repo_builder.py
@@ -1,13 +1,13 @@
 import uuid
 from faker import Faker
-
+fake = Faker()
 
 class Builder:
     @staticmethod
     def build_repo():
         repository = {
             'name': uuid.uuid4().hex,
-            'description': uuid.uuid4().hex,
+            'description': fake.text(),
             'homepage': 'https://github.com',
             'private': False,
             'has_issues': True,

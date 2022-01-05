84fdcde81b732ff327434c3f97cad271d1a16fee
ElevateStuff <33041794+ElevateStuff@users.noreply.github.com>
2022-01-04 19:20:45 -0700

    add datatable example

--- test/features/github_repos_api.feature
+++ test/features/github_repos_api.feature
@@ -10,3 +10,13 @@ Feature: GitHub API
 Scenario: GET Call returns 200
 	Given the user executes a GET User call
 	Then the response status code is "200"
+
+
+@smoke
+Scenario Outline: Create a new Repo using a Data Table
+    Given the user creates a new repo with a <name>, <description>, <homepage>, <is_private>
+    Then the repo is created
+
+    Examples:
+    | name        | description | homepage       | is_private |
+    | random_name | description | www.google.com | False      |
--- test/step_defs/test_repos_steps.py
+++ test/step_defs/test_repos_steps.py
@@ -1,11 +1,18 @@
-from conftest import USER, HEADER, AUTH
-from pytest_bdd import scenarios, given, then, parsers
+from conftest import USER, HEADER, AUTH, USER_REPOS
+from pytest_bdd import scenarios, given, then, parsers, scenario
 from pytest_check import equal
 from test.helpers import request_helpers
 
 # Scenarios
 
 scenarios('../features/github_repos_api.feature')
+
+# @scenario(
+#     "../features/github_repos_api.feature",
+#     "Create a new Repo using a Data Table"
+# )
+# def test_outlined():
+#     pass
 
 # Given
 
@@ -15,8 +22,28 @@ def get_user(urls):
     print(response.content)
     return response
 
+#in order to pass in data from the datatable you must use a given parser first then a second given to pass the parsed values into.
+@given(parsers.parse('the user creates a new repo with a {name}, {description}, {homepage}, {is_private}'))
+@given('the user creates a new repo with a <name>, <description>, <homepage>, <is_private>', target_fixture="create_repo")
+def create_repo(urls, name, description, homepage, is_private):
+    payload = {
+        'name': name,
+        'description': description,
+        'homepage': homepage,
+        'is_private': is_private
+    }
+    print(payload)
+    response = request_helpers.post(urls['githubBaseURL'] + USER_REPOS, headers=HEADER, auth=AUTH, data=payload)
+    return response
+
+
 # Then
 
 @then(parsers.parse('the response status code is "{code:d}"'))
 def get_user_200(get_user, code):
     equal(get_user.status_code, code)
+
+
+@then('the repo is created')
+def repo_created(create_repo):
+    equal(create_repo.status_code, '201')

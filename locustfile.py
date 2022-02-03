2b05508b57e3384ce067805a22eaeaa4af039266
ElevateStuff <33041794+ElevateStuff@users.noreply.github.com>
2022-02-02 20:24:48 -0700

    Fix run authorization once by using requests coupled with test_start listener

--- locustfile.py
+++ locustfile.py
@@ -1,54 +1,59 @@
-from locust import HttpUser, task, constant, TaskSet
+from locust import HttpUser, task, constant, User, events
+import requests
 from faker import Faker
+
 
 # Command line statement to execute locust in the Terminal or Powershell
 # locust --headless --users 10 --spawn-rate 1 --host https://test-api.k6.io  --run-time 5 --html locust_result.html --logfile locust_log.log
 
 
+# Run Test Start Listener and Requests library to retrieve the initial token
+
+
+@events.test_start.add_listener
+def _(environment, **kwargs):
+    # Set token global variable to be used by subsequent calls
+    global token
+    token = authorization(environment.host)
+
+def authorization(host):
+    # Create Fake data for user creation
+    fake = Faker()
+    first_name = fake.name()
+    last_name = fake.name()
+    user_name = fake.email()
+    password = fake.name()
+
+    # Create a user
+    json_user = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'username': user_name,
+        'password': password
+    }
+
+    requests.post(host + '/user/register/', json=json_user)
+
+    json_creds = {
+        'username': user_name,
+        'password': password
+    }
+
+    # Retrieve bearer token from response
+    response = requests.post(host + '/auth/token/login/', json=json_creds)
+
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        print('Response JSON', response.json(), 'JSON output.')
+        exit()
+
+    data = response.json()
+    return data['access']
+
+
 class LocustK6API(HttpUser):
     wait_time = constant(2)
-
-    def on_start(self):
-        self.authorization()
-
-    def authorization(self):
-        # Set token global variable to be used by subsequent calls
-        global token
-
-        # Create Fake data for user creation
-        fake = Faker()
-        first_name = fake.name()
-        last_name = fake.name()
-        user_name = fake.email()
-        password = fake.name()
-
-        # Create a user
-        json_user = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'username': user_name,
-            'password': password
-        }
-
-        self.client.post('/user/register/', json=json_user)
-
-        json_creds = {
-            'username': user_name,
-            'password': password
-        }
-
-        # Retrieve bearer token from response
-        response = self.client.post('/auth/token/login/', json=json_creds)
-
-        # Check for HTTP codes other than 200
-        if response.status_code != 200:
-            print('Status:', response.status_code, 'Problem with the request. Exiting.')
-            print('Response JSON', response.json(), 'JSON output.')
-            exit()
-
-        data = response.json()
-        token = data['access']
-        return token
 
     @task
     def get_public_crocodiles(self):
@@ -59,4 +64,3 @@ class LocustK6API(HttpUser):
     def get_my_crocodiles(self):
         headers = {'Authorization': 'Bearer ' + token}
         self.client.get('/my/crocodiles/', headers=headers)
-
